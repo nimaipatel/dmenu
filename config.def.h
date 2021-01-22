707414651b12dd85360d7ce5f260a4ff99cdf326
nimai <nimai.m.patel@gmail.com>
2021-01-22 22:45:36 +0530

    moondlt colors 🪰

--- config.def.h
+++ config.def.h
@@ -11,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#b2b2b2", "#373c40" },
+	[SchemeSel]  = { "#b2b2b2", "#ae81ff" },
+	[SchemeOut]  = { "#b2b2b2", "#373c40" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
