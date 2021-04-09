2bd5436abe3c9492e3a390e69442a50b1fa923d4
nimai <nimai.m.patel@gmail.com>
2021-04-09 14:38:10 +0530

    use monospace at compile time, and let xresources handle font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Fixed:size=13",
+	"monospace:size=13",
 	"Joypixels:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
