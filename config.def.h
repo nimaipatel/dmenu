263ebcac0cfd0fb45e785ced4e3ffd29a6199a39
nimai <nimai.m.patel@gmail.com>
2021-04-22 21:04:44 +0530

    fallback with NF just in case

--- config.def.h
+++ config.def.h
@@ -6,7 +6,8 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=13",
-	"Joypixels:size=13"
+	"Joypixels:size=13",
+	"Symbols Nerd Font:size=13",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
