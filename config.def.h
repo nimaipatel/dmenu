1e6067efc951a5d96adab3ee2bd4012850a0ff40
nimai <nimai.m.patel@gmail.com>
2021-04-24 07:26:07 +0530

    restore to original settings, and set rely on Xresources

--- config.def.h
+++ config.def.h
@@ -12,9 +12,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	             /*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#928374" },
-	[SchemeSel]  = { "#ebdbb2", "#b16286" },
-	[SchemeOut]  = { "#ebdbb2", "#928374" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
