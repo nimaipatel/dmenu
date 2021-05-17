5ff326b206eac2a131ac209663cfe34120bb677a
nimai <nimai.m.patel@gmail.com>
2021-05-17 12:09:27 +0530

    reduced font size

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=13",
-	"Joypixels:size=13",
-	"Symbols Nerd Font:size=13",
+	"monospace:size=8",
+	"Joypixels:size=8",
+	"Symbols Nerd Font:size=8",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -19,8 +19,8 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 27;
-static unsigned int min_lineheight = 27;
+static unsigned int lineheight = 20;
+static unsigned int min_lineheight = 20;
 
 /*
  * Characters not considered part of a word while deleting words
