8c11fc3728562fa18d197e63701bba02e72771ec
nimai <nimai.m.patel@gmail.com>
2021-10-24 18:52:25 +0530

    Non feat changes

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=8",
+	"monospace:size=12",
 	"Joypixels:size=8",
 	"Symbols Nerd Font:size=8",
 };
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 20;
 static unsigned int min_lineheight = 20;
